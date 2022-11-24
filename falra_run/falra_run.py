906efb5dc2516cdfeaf9a63f568a5d059d4c4514
bohachu <cbh@cameo.tw>
2022-11-24 11:42:34 +0800

    falra_run     github         git_caro             add.py                 def add(a,b):         git_jc             multiply.py                 def multiply(a,b):
    執行的時候會切換路徑到正確可以 import 的路徑，動態匯入，動態執行

--- falra_run/falra_run.py
+++ falra_run/falra_run.py
@@ -1,6 +1,35 @@
-def hi():
-    print('hi falra_run')
+import uvicorn
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 
-if __name__ == "__main__":
-    hi()
+def init_app():
+    global app
+    app = FastAPI()
+    app.add_middleware(CORSMiddleware,
+                       allow_origins=['*'],
+                       allow_credentials=True,
+                       allow_methods=['*'],
+                       allow_headers=['*'])
+    Path('web').mkdir(parents=True, exist_ok=True)
+    app.mount('/web', StaticFiles(directory='web'), name='web')
+
+
+init_app()
+
+
+@app.get('/')
+def root():
+    return "Hi Falra!"
+
+
+@app.post("/api/falra_run/")
+async def falra_run(request: Request):
+    return await request.json()
+
+
+# uvicorn falra_run.falra_run:app --port 8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
